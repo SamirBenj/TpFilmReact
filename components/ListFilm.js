c9fc5ba5ebe05dd30a1ba5563e087373baf2eed1
laure <laurent.cocheteux@hotmail.fr>
2022-02-03 12:12:40 +0100

    new code

--- components/ListFilm.js
+++ components/ListFilm.js
@@ -1,59 +1,62 @@
 import React, { useState } from "react";
-import View from "react-native";
+import { View, FlatList, Pressable } from "react-native";
 
 const ListFilm = () => {
-    const [isLoading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
-    const [isShowWidget, setShowData] = useState();
-    const url = "http://www.omdbapi.com/?s=${myValueFilm}&apikey=5edd8232'";
-    const showData = () => {
-        if (isShowWidget == true) {
-            console.log('true')
-            setShowData(false);
-        }
-        else {
-            console.log('false');
-            setShowData(true);
-        }
-    }
-    useEffect(() => {
 
-        const fetchCryptoData = async () => {
-            try {
-                const response = await axios.get(url, {
-                    headers: {
-                        'X-CMC_PRO_API_KEY': '5edd8232',
-                        "Accept": "application/json",
-                    },
-                })
-                    .then((response) => response.json())
-                    .then((json) => setData(json.data))
-                    .catch((error) => console.error(error))
-                    .finally(() => setLoading(true));
-
-            } catch (e) {
-                console.log(e);
+    const ListFilm = ({ navigation }) => {
+        const [isLoading, setLoading] = useState(false);
+        const [data, setData] = useState([]);
+        const [isShowWidget, setShowData] = useState();
+        const url = "http://www.omdbapi.com/?s=${myValueFilm}&apikey=5edd8232'";
+        const showData = () => {
+            if (isShowWidget == true) {
+                console.log('true')
+                setShowData(false);
+            }
+            else {
+                console.log('false');
+                setShowData(true);
             }
         }
-        fetchCryptoData();
-    }, []);
+        useEffect(() => {
 
-    return (
-        <View>
-            <FlatList
-                data={data ?? 0}
-                renderItem={(item) =>
-                    <Pressable
-                        onPress={() => {
-                            navigation.navigate('DetailsFilm', {
+            const fetchCryptoData = async () => {
+                try {
+                    const response = await axios.get(url, {
+                        headers: {
+                            'X-CMC_PRO_API_KEY': '5edd8232',
+                            "Accept": "application/json",
+                        },
+                    })
+                        .then((response) => response.json())
+                        .then((json) => setData(json.data))
+                        .catch((error) => console.error(error))
+                        .finally(() => setLoading(true));
 
-                            }
-                            )
-                        }}>
-                    </Pressable>
-                }>
-            </FlatList>
-        </View>
-    );
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+            fetchCryptoData();
+        }, []);
 
+        return (
+            <View>
+                <FlatList
+                    data={data ?? 0}
+                    renderItem={(item) =>
+                        <Pressable
+                            onPress={() => {
+                                navigation.navigate('DetailsFilm', {
+
+                                }
+                                )
+                            }}>
+                        </Pressable>
+                    }>
+                </FlatList>
+            </View>
+        );
+
+    }
 }
