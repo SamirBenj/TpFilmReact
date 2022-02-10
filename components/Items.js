c60226585aed177c59973af939de84c0523113be
laure <laurent.cocheteux@hotmail.fr>
2022-02-10 16:33:16 +0100

    news

---
+++ components/Items.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const ItemRow = ({ item }) => {
+    return (
+        <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            padding: 20,
+            margin: 20,
+            width: 300,
+            borderRadius: 30,
+            backgroundColor: '#3DA6FF',
+        }}>
+
+            <Image
+                style={styles.afficheFilm}
+                source={{ uri: `https://s2.coinmarketcap.com/static/img/coins/64x64/${item.item.imdbID}.png` }}></Image>
+            <View style={{ flexDirection: 'column', padding: 10, borderRadius: 20 }}>
+                <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>{item.item.Title}</Text>
+                <Text style={{ color: 'orange', fontWeight: 'bold', fontSize: 20 }}>{item.item.Year}</Text>
+                <Text style={{ color: 'orange', fontWeight: 'bold', fontSize: 20 }}>{item.item.Type}</Text>
+                <Text style={{ color: 'orange', fontWeight: 'bold', fontSize: 20 }}>{item.item.Poster}</Text>
+
+            </View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    afficheFilm: {
+        width: 40,
+        height: 40,
+
+
+    }
+});
+export default ItemRow;
--- components/ListFilm.js
+++ components/ListFilm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { View, FlatList, Pressable } from "react-native";
-
+import { View, FlatList, Pressable, StyleSheet } from "react-native";
+import ItemRow from "./Items";
 
 const ListFilm = ({ navigation }) => {
     const [isLoading, setLoading] = useState(true);
@@ -25,20 +25,49 @@ const ListFilm = ({ navigation }) => {
     }, []);
 
     return (
-        <View>
-            {isLoading ? <Text>chargement en cours ....</Text> :
-                <FlatList
-                    renderItem={(item) =>
-                        <Pressable
-                            onPress={() => {
-                                navigation.navigate('DetailsFilm', {});
-                            }}>
-                        </Pressable>
-                    }>
-                </FlatList>}
+        <View style={styles.mainContainer}>
+            <View style={styles.card}>
+            </View>
+            <View style={styles.bottomContainer}>
+                <View style={{ flex: 1, padding: 30 }}>
+                    {isLoading ? <Text>chargement en cours ....</Text> :
+                        <FlatList
+                            renderItem={(item) =>
+                                <Pressable
+                                    onPress={() => {
+                                        navigation.navigate('DetailsPage', {
+                                            id: item.item.imdbID,
+                                            nomFilm: item.item.Title,
+                                            anneeFilm: item.item.Year,
+                                            genreFilm: item.item.Type
+                                        }
+                                        )
+                                    }}>
+                                    <ItemRow item={item}></ItemRow>
+                                </Pressable>
+                            }>
+                        </FlatList>
+                    }
+                </View>
+            </View>
         </View>
     );
-
 }
+
+
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: 'blue',
+    },
+    card: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        shadowColor: 'black',
+        padding: 30,
+
+    }
+})
 
 export default ListFilm
